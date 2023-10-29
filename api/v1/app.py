2f9377f5fa2255f9a152dab91687ea038e0a82f1
TeeMeeLehin <samemerald8@gmail.com>
2023-10-29 20:39:18 +0100

    pycodestyle fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,6 +9,7 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix="/api/v1")
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """closes the storage on teardown"""
@@ -18,5 +19,5 @@ def teardown_db(exception):
 if __name__ == '__main__':
     host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
     port = int(os.environ.get('HBNB_API_PORT', 5000))
-    
+
     app.run(host=host, port=port, threaded=True)
